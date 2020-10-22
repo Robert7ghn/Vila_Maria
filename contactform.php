04d6856b70c5c88bac48ea18ddc88b990776e738
Robert7ghn <florin1971ro@gmail.com>
2020-10-22 16:02:56 +0000

    added php file

---
+++ contactform.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST['SUBMIT'])) {
+	$name = $_POST['name'];
+	$subject = $_POST['subject'];
+	$mailFrom = $_POST['mail'];
+	$message = $_POST['message'];
+	
+	$mailTo = "robert1971ro@gmail.com";
+	$headers = "From: ".$mailFrom;
+	$txt = "You have received an e-mail from ".$name.".\n\n".$message;
+	
+	
+	
+	mail($mailTo,$subject, $txt, $headers);
+    header("Location:index.php?mailsend");
+}
+
+
