47d645a6dce51578b79cafe4c08d50b76686466d
Robert7ghn <florin1971ro@gmail.com>
2020-10-24 13:06:31 +0000

    new pict and c-form chngd

--- contactform.php
+++ contactform.php
@@ -5,12 +5,13 @@ if (isset($_POST['submit']))
     $name = $_POST['name'];
 	$mailFrom = $_POST['email'];
 	
-	$mailTo = "maria@vila-maria.online";
+    $mailTo = "maria@vila-maria.online";
+    $subject "Subject: ".subject;
+    $message = " ";
 	$headers = "From: ".$mailFrom;
 	$txt = "You have received an e-mail from ".$name;
 	
-	
-    mail($mailTo, $txt, $headers);
+    mail($mailTo, $subject, $message, $headers, $txt);
 
 }
 ?>
