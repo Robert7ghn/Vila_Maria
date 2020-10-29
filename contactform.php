120db9add4ad34901d02438df8b44f6b9d675bf3
Robert7ghn <florin1971ro@gmail.com>
2020-10-29 07:41:24 +0000

    mod af.php

--- contactform.php
+++ contactform.php
@@ -1,17 +1,27 @@
 <?php
-
-if (isset($_POST['submit'])) 
-{
-    $name = $_POST['name'];
-	$mailFrom = $_POST['email'];
+	$name = $_POST['name'];
+	$visitor_email = $_POST['email'];
+	$message = $_POST['mesage'];
 	
-    $mailTo = "maria@vila-maria.online";
-    $subject "Subject: ".subject;
-    $message = " ";
-	$headers = "From: ".$mailFrom;
-	$txt = "You have received an e-mail from ".$name;
 	
-    mail($mailTo, $subject, $message, $headers, $txt);
+	$email_from = "$visitor_email";
+	
+	$email_subject = "Cerere-Rezervare - ";
+	
+	$email_body = "User Name: $name.\n".
+					"User Email: $visitor_email.\n".
+						"User Message: $message.\n";
+						
+						
+	$to ="maria@vila-maria.online";					
+	
+	$headers ="From: $email_from \r\n";
+	
+	
+	$headers ="Reply-To: $visitor_email \r\n";
+	
+	mail($to,$email_subject,$email_body,$headers);
+	
+	header("Location:index.html");
 
-}
 ?>
