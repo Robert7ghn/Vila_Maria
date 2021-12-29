984187c8158f7c8d2d35195243ef869fa0782bd1
Robert7ghn <71172526+Robert7ghn@users.noreply.github.com>
2021-12-29 13:43:54 +0000

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,26 +1,41 @@
 <?php
-	$name = $_POST['name'];
-	$visitor_email = $_POST['email'];
-	$message = $_POST['message'];
+	$name=$_POST['name'];
+	$visitor_email=$_POST['email'];
+
+//Recipient						
+	$to="maria@vila-maria.ro";					
 	
-	$email_from = "$visitor_email";
+//Subject
+	$email_subject="Cerere-Rezervare";
 	
-	$email_subject = "Cerere-Rezervare";
-	
-	$email_body = "User Name: $name.\n".
-					"User Email: $visitor_email.\n".
-						"User Message: $message.\n";
-						
-						
-	$to ="maria@vila-maria.online";					
-	
-	$headers ="From: $email_from \r\n";
+//Message
+	$message=$_POST['period'];
+	$message2=$_POST['nr-pers'];
+	$message3=$_POST['nr-cam'];
+	$message4=$_POST['nr-nopti'];
+	$message5=$_POST['telef'];
+	$message6=$_POST['address'];
 	
 	
-	$headers ="Reply-To: $visitor_email \r\n";
+	$email_from="$visitor_email";
+
+//Email body	
+	$email_body="Nume Client: $name \r\n".
+					"Email Client: $visitor_email \r\n".
+					"Adresa: $message6 \r\n".
+					"Telefon: $message5 \r\n".
+						"Perioada rezervare: $message \r\n".
+						"Nr.persoane: $message2 \r\n".
+						"Nr.camere: $message3 \r\n".
+						"Nr.nopti: $message4 \r\n";
+
+//Headers	
+    $headers="Content-type: text/html\r\n";
+	$headers="From: $email_from\r\n";
+	$headers="Reply-To: $visitor_email\r\n";
 	
+//Send email
 	mail($to,$email_subject,$email_body,$headers);
-	
 	header("Location:index.html");
 
 ?>
